a93c2c6b6b0fc02bcea9836cccd276f5a004fb69
Allen Liang <jor-wei@hotmail.com>
2020-11-05 20:32:55 +1300

    master

--- lens_cuda.cu
+++ lens_cuda.cu
@@ -28,7 +28,7 @@ const float YL2 =  WL;
 
 // Implement lens equation, given the lens position (xl, yl) and the
 // lens system configuration, shoot a ray back to the source position
-// (xs, ys)
+// (xs, ys) device function to be called by kernel function
 __device__ void shoot(float& xs, float& ys, float xl, float yl, 
   float* xlens, float* ylens, float* eps, int nlenses)
 {
@@ -45,7 +45,6 @@ __device__ void shoot(float& xs, float& ys, float xl, float yl,
 
 }
 
-
 // Used to time code. OK for single threaded programs but not for
 // multithreaded programs. See other demos for hints at timing CUDA
 // code.
@@ -56,6 +55,10 @@ double diffclock(clock_t clock1,clock_t clock2)
   return diffms; // Time difference in milliseconds
 }
 
+// Kernel function to be called by CPU and run on GPU
+// Draw the lensing image map here. For each pixel, shoot a ray back
+// to the source plane, then test whether or or not it hits the
+// source star
 __global__ void mx_shoot(float* xlens, float* ylens, float* eps, float* d_lensim, float XL1, float YL1, int nlenses, float lens_scale, int cols, int rows) 
 {
     // Source star parameters. You can adjust these if you like - it is
@@ -107,7 +110,7 @@ int main(int argc, char* argv[])
   // Pixel size in physical units of the lens image. You can try finer
   // lens scale which will result in larger images (and take more
   // time).
-  const float lens_scale = 0.003;
+  const float lens_scale = 0.001;
 
   // Size of the lens image
   const int npixx = static_cast<int>(floor((XL2 - XL1) / lens_scale)) + 1;
@@ -122,9 +125,8 @@ int main(int argc, char* argv[])
   float *d_xlens, *d_ylens, *d_eps, *d_lensim;
   size_t size = nlenses * sizeof(float);
   size_t size_img = npixx * npixy * sizeof(float);
-  size_t pitch;
-  //float* h_lensim = (float*)malloc(size_img);
 
+  // create the variable of the GPU
   cudaMalloc(&d_xlens, size);
   cudaMalloc(&d_ylens, size);
   cudaMalloc(&d_eps, size);
@@ -133,9 +135,10 @@ int main(int argc, char* argv[])
   cudaMemcpy(d_xlens, xlens, size, cudaMemcpyHostToDevice);
   cudaMemcpy(d_ylens, ylens, size, cudaMemcpyHostToDevice);
   cudaMemcpy(d_eps, eps, size, cudaMemcpyHostToDevice);
-  //cudaMemcpy(d_lensim, lensim.buffer, size_img, cudaMemcpyHostToDevice);
+  // instead of copy from lensim, just use cudaMemset to set the initial value
   cudaMemset(d_lensim, 0.0, size_img);
 
+  // assign the parameter of the thread size and grid size
   int total = npixx * npixy;
   int threadsPerBlock = 512;
   int blocksPerGrid = (total + threadsPerBlock - 1) / threadsPerBlock;
@@ -149,37 +152,6 @@ int main(int argc, char* argv[])
   std::cout << "# Time elapsed: " << tms << " ms " << std::endl;
 
   cudaMemcpy(lensim.buffer, d_lensim, size_img, cudaMemcpyDeviceToHost);
-  //cudaMemcpy(h_lensim, d_lensim, size_img, cudaMemcpyDeviceToHost);
-
-  // Draw the lensing image map here. For each pixel, shoot a ray back
-  // to the source plane, then test whether or or not it hits the
-  // source star
-  // replace with device function
-  /*const float rsrc2 = rsrc * rsrc;
-  float xl, yl, xs, ys, sep2, mu;
-  float xd, yd;
-  int numuse = 0;
-  for (int iy = 0; iy < npixy; ++iy) 
-  for (int ix = 0; ix < npixx; ++ix) { 
-
-    yl = YL1 + iy * lens_scale;
-    xl = XL1 + ix * lens_scale;
-
-    shoot(xs, ys, xl, yl, xlens, ylens, eps, nlenses);
-
-    xd = xs - xsrc;
-    yd = ys - ysrc;
-    sep2 = xd * xd + yd * yd;
-    if (sep2 < rsrc2) {
-      mu = sqrt(1 - sep2 / rsrc2);
-      lensim(iy, ix) = 1.0 - ldc * (1 - mu);
-    }
-  }*/
-
-  // for (int iy = 0; iy < npixy; ++iy) 
-  // for (int ix = 0; ix < npixx; ++ix) { 
-  //   lensim(iy, ix) = h_lensim[iy * npixy + ix];
-  // }
 
   // Write the lens image to a FITS formatted file. You can view this
   // image file using ds9
